c003a7cb7e4075a0ac3f7cbb4b1c443390b01426
Yegor Levandovskyi <yegor.levandovskyi@interactio.io>
2022-01-18 19:09:15 +0200

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,27 @@ import {useState} from "react";
 import "./App.css";
 import {Component1} from "./components/Component1";
 import {CounterContext} from "./context/counter";
+import {WordContext} from "./context/word";
 
 function App() {
     const [counter, setCounter] = useState(0);
+    const [word, setWord] = useState("");
+    const handleChange = (e) => {
+        setWord(e.target.value);
+    };
 
     return (
         <div className="App">
             <CounterContext.Provider value={{counter, setCounter}}>
-                <Component1 />
+                <WordContext.Provider value={word}>
+                    <Component1 />
+                </WordContext.Provider>
             </CounterContext.Provider>
+
+            <div>
+                <label htmlFor="word">Word: </label>
+                <input name="word" value={word} onChange={handleChange} />
+            </div>
         </div>
     );
 }
--- src/components/Component2.jsx
+++ src/components/Component2.jsx
@@ -1,14 +1,14 @@
 import {useContext} from "react";
 import {CounterContext} from "../context/counter";
+import {WordContext} from "../context/word";
 
 export const Component2 = () => {
     const {counter, setCounter} = useContext(CounterContext);
-
+    const word = useContext(WordContext);
     return (
         <div>
-            <div>
-                counter is: {counter}
-            </div>
+            <div>counter is: {counter}</div>
+            <div>word is: {word}</div>
             <button
                 onClick={() => {
                     setCounter((prev) => prev + 1);
